408e032327aa606fa86c0b53f60c983034d2fcc1
Lowrance, Peter <PLowrance@veritegroup.com>
2022-01-17 13:18:29 -0500

    Add react-website

---
+++ website/react-website/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const isProduction = process.env.npm_lifecycle_script.includes('production');
+
+module.exports = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'source-map',
+    entry: {
+        'index': path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'static/'),
+        filename: 'index.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        ['@babel/preset-react', {"runtime": "automatic"}]
+                    ]
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(svg|ico|png|gif|jpg|jpeg)(\?|$)/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    cache: true
+}
